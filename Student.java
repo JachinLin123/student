be9fd13d4ea222f0ece463a76c2593c9520772b2
JachinLin <169580473@qq.com>
2019-03-27 13:38:37 +0800

    studentmanage

---
+++ Student.java
@@ -0,0 +1,41 @@
+package ArrayListTest;
+
+/**
+ * @ mumu
+ * data:2019/3/22 17:39
+ * 学生集合的练习。
+ */
+
+public class Student {
+    private String id;
+    private String name;
+    private String age;
+    private String Adress;
+    public String getAdress() {
+        return Adress;
+    }
+    public void setAdress(String adress) {
+        Adress = adress;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+
+}
